8a3a4c72845f6a7beb5c19ee393e4f143e6e8402
TsukiGva2 <tsukigva@gmail.com>
2024-07-08 00:36:10 -0300

    Using readline for completion support

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from typing import final, override, Final
 import subprocess
+import readline
 import sys
+
 
 @final
 class ReadLine:
-    def __init__(self, prompt="> ", non_empty=True):
-        while (self.line := input(prompt)) == "" and non_empty: ...
+    def __init__(self, prompt="> ", words=[], non_empty=True):
+        while (line := input(prompt)) == "" and non_empty: ...
+        self.line = line
 
     def __str__(self):
         return self.line
+
+
+class CommandNotFound(FileNotFoundError): ...
+
 
 @final
 class Command:
@@ -16,14 +23,27 @@ class Command:
         self.cmdline: str = cmdline
         self.args: list[str] = cmdline.split()
 
+        self.process: subprocess.Popen = None
+
     def execute(self) -> int:
         """
-        Run command and return 
+        Run command and return the status code
         """
         if self.cmdline == "":
             raise ValueError("Can't run empty command")
 
-        subprocess.Popen()
+        try:
+            self.process = (
+                subprocess.Popen(
+                    self.args,
+                    #stderr = subprocess.PIPE
+                )
+            )
+        except FileNotFoundError:
+            raise CommandNotFound("Command not found")
+
+        return self.process.wait()
+
 
 class ConditionalREPL:
     """
@@ -36,6 +56,7 @@ class ConditionalREPL:
     keep running under a certain
     condition
     """
+
     def __init__(self): ...
 
     def read(self): ...
@@ -54,16 +75,20 @@ class ConditionalREPL:
 
     def condition(self): ...
 
+
 @final
 class Shell(ConditionalREPL):
     def __init__(self):
-        self.command: Command = Command(None)
+        self.command: Command = Command()
         self.prompt: str = "- "
-        self.status: int = 0 # TODO: enum like
+        self.status: int = 0  # TODO: enum like
 
     @override
     def eval(self):
-        self.status = self.command.execute()
+        try:
+            self.status = self.command.execute()
+        except CommandNotFound:
+            sys.stderr.write("  ^^^ Command not found\n")
 
     @override
     def read(self):
@@ -72,20 +97,27 @@ class Shell(ConditionalREPL):
 
     @override
     def condition(self) -> bool:
-        return self.status > 0
+        return self.status == 0
+
 
 def main():
+    readline.parse_and_bind("tab: complete")
+    readline.set_completer_delims(" \t\n`~!@#$%^&*()-=+[{]}\\|;:\'\",<>/?")
+
     shell: Final[Shell] = Shell()
 
     try:
         shell.loop()
-    except ShellException as err:
-        print(f"EXCEPTION:\n\t| '{err}'")
+    except EOFError:
+        print("\nEOF")
+        return True
+    except Exception as err:
+        print(f"\n| Exception: '{err}'")
         return False
 
     return True
 
+
 if __name__ == "__main__":
     if not main():
         sys.exit(1)
-
