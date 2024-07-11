4ba75f496914843e2a487d27df66778409103640
TsukiGva2 <tsukigva@gmail.com>
2024-07-11 17:30:12 -0300

    Progress on defining the language further

--- forth/wordtable.py
+++ forth/wordtable.py
@@ -1,4 +1,5 @@
 from .configuration import HEADER_EMPTY
+from .std import StdFunctions
 
 
 class WordHeader:
@@ -16,48 +17,180 @@ class WordHeader:
     def insert(self, instruction):
         self.instructions.append(instruction)
 
-import .std as forth_std
+
+# FMT
+# '(file name \.) fmt
+
+"""
+    # (filename -- msg)     # specifying arguments (optional but recommended)
+    : exists?               # defining a function
+
+        # setting a return message
+
+        '(file exists)      # push return message to the stack
+        swap                # swap return message and file name
+
+        dup                 # duplicating file name so we can still use it later
+
+        '(no such file \.)  # push error message to the stack
+        fmt                 # formatting (substitutes any \. in the string with a value from the stack)
+
+        swap                # swap return message and file name
+
+
+        # building a command
+
+        '(stat \.)          # adding '(stat) to the stack
+        fmt
+
+
+        # running the command
+
+        ?                   # run 'stat' command and get return value
+
+
+        # conditional execution
+
+        =>                  # if return value is a success status then
+
+            swap            # swap return message ('no such file' was pushed last so we gotta switch)
+
+        end                 # end if statement
+
+        dup                 # duplicating the return value (so we can print it and still return)
+        .                   # print return message (top element on the stack)
+
+    ;                       # ending function definition
+
+    # dummy example
+    : writesafe ( file str -- )
+        swap
+        exists? '(file exists) =
+        =>
+            '()
+        end
+    ;
+"""
+
+# - ls
+# .git/ main.py
+# - 'main.py exists?
+# [main.py, file exists]
+# [file exists, main.py]
+# [file exists, main.py, main.py, no such file]
+# [file exists, main.py, no such file main.py]
+# [file exists, no such file main.py, main.py]
+# [file exists, no such file main.py, main.py, stat \.]
+# [file exists, no such file main.py, stat main.py]
+# [file exists, no such file main.py, 0 (SUCCESS)]
+# [no such file main.py, file exists]
+# [no such file main.py, file exists, file exists]
+# [no such file main.py, file exists]
+# OK. FILE EXISTS.
+
+
+# read -> input()
+
+# + -> push(): pop() + pop()
+# - -> push(): pop() - pop()
+# / -> push(): pop() / pop()
+# * -> push(): pop() * pop()
+# ^ -> push(): pop() ^ pop()
+# ~ -> push(): ~ pop()
+
+# = -> push(): pop() == pop()
+# ~= -> push(): pop() != pop()
+# > -> push(): pop() > pop()
+# < -> push(): pop() < pop()
+# >= -> push(): pop() >= pop()
+# <= -> push(): pop() <= pop()
+
+# => -> if not pop() then state = SKIP
+# +noskip end -> state = INTERPRET
+
+"""
++noskip
+: or
+    state?
+;
+"""
+
+# $ -> push(): ( getenv(): ENV=pop() )
+# export -> push(): ( setenv(): ENV=pop() VAL=pop() )
+
+# TODO: these commands need a PIPE
+
+"""
+PROTO SHELL -> [TODO:shell.run()] as Run()
+
+: SHELL ( PIPE cmd -- )
+    swap
+    dup
+
+    pipe? => swap Run(): pop() pop()
+    or Run(): pop()
+;
+"""
+
+# ? -> SHELL():
+
+"""
+2 2 =
+=> INTERPRET
+
+    1 2 => SKIP
+    end INTERPRET
+
+    3 3 => INTERPRET
+
+        2 4 => SKIP
+
+        end INTERPRET
+
+        4 4 => INTERPRET
+
+        end INTERPRET
+
+    end INTERPRET
+
+end INTERPRET
+"""
+
 
 class WordTable:
     def __init__(self):
         self.words = {
-                # IO
-                   ".": forth_std.put(),
-                "read": forth_std.ask(),
-
-                # operators
-                "+": forth_std.add(),
-                "-": forth_std.sub(),
-                "/": forth_std.div(),
-                "*": forth_std.mul(),
-                "^": forth_std.pow(),
-                "~": forth_std.non(),
-
-                # logic
-                 "=": forth_std.equals(),
-                "~=": forth_std.nequal(),
-                 ">": forth_std.greater(),
-                 "<": forth_std.lesser(),
-                ">=": forth_std.geq(),
-                "<=": forth_std.leq(),
-
-                 "=>": forth_std.implies(),
-
-                # environment
-                "$": forth_std.env(),
-                "export": forth_std.setenv(),
-
-                # Shell
-                 "?": forth_std.shell(),
-                 "|": forth_std.pipe(),
-                "->": forth_std.tofile(),
-
-                # State
-                "@": forth_std.fetchvar(),
-                "!": forth_std.definevar(),
-
-                # Misc
-                "-.": forth_std.pop()
+            # IO
+            # ".": StdFunctions.put(), # : . '(echo) ? ;
+            "read": StdFunctions.ask(),
+            # operators
+            "+": StdFunctions.add(),
+            "-": StdFunctions.sub(),
+            "/": StdFunctions.div(),
+            "*": StdFunctions.mul(),
+            "^": StdFunctions.pow(),
+            "~": StdFunctions.non(),
+            # logic
+            "=": StdFunctions.equals(),
+            "~=": StdFunctions.nequal(),
+            ">": StdFunctions.greater(),
+            "<": StdFunctions.lesser(),
+            ">=": StdFunctions.geq(),
+            "<=": StdFunctions.leq(),
+            "=>": StdFunctions.implies(),
+            "end": StdFunctions.end(),
+            # environment
+            "$": StdFunctions.env(),
+            "export": StdFunctions.setenv(),
+            # Shell
+            "?": StdFunctions.shell(),
+            "|": StdFunctions.pipe(),
+            "<-": StdFunctions.tofile(),
+            # State
+            "@": StdFunctions.fetchvar(),
+            "!": StdFunctions.definevar(),
+            # Misc
+            "-.": StdFunctions.pop(),
         }
 
         self.header = WordHeader()
